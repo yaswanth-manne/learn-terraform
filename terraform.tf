df1f3cb74896b5f779e42299c0aab14175c84a75
yaswanth-manne <yaswanth2228@gmail.com>
2024-07-15 13:39:41 +0530

    Remove cloud block

--- terraform.tf
+++ terraform.tf
@@ -2,15 +2,15 @@
 # SPDX-License-Identifier: MPL-2.0
 
 terraform {
-
+/*
   cloud {
-    organization = "organization-name"
+    organization = "learn-terraform-docs"
 
     workspaces {
       name = "learn-terraform"
     }
   }
-
+*/
   required_providers {
     aws = {
       source  = "hashicorp/aws"
